b72d7823143931a491d2cbe31e74d0e83eda49af
StarLukam <drekosuuu@gmail.com>
2026-05-04 16:49:50 +0700

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,6 @@
+import type { NextConfig } from "next";
+const nextConfig: NextConfig = {
+  images: { remotePatterns: [{ protocol: "https", hostname: "**" }] },
+  eslint: { ignoreDuringBuilds: true },
+};
+export default nextConfig;
