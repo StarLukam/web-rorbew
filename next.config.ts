c4167d60b472a6858ef7532568bda57f54c10e06
StarLukam <drekosuuu@gmail.com>
2026-05-06 19:02:36 +0700

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,6 +0,0 @@
-import type { NextConfig } from "next";
-const nextConfig: NextConfig = {
-  images: { remotePatterns: [{ protocol: "https", hostname: "**" }] },
-  eslint: { ignoreDuringBuilds: true },
-};
-export default nextConfig;
